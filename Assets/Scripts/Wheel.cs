0a7943e149098cb4f872bd7ba3c27b131794b522
Himansu Negi <100259622+Himanshu-N@users.noreply.github.com>
2024-07-12 20:40:21 +0530

    Added an extra Environment and a better Car
    Environment from > SimplePoly City - Low Poly Assets
    Added mesh collider to the environment since the models were missing colliders.
    
    Transferred the car mechanics to a new car imported from FINAL FORM STUDIO> Sedan car - 01
    
    Added a skybox and a outer boundary to the scene.

--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -12,19 +12,22 @@ public class Wheel : MonoBehaviour
     public float SteerAngle { get; set; }
 
     private WheelCollider wheelCollider;
-    private Transform wheelTransform;
+    private MeshRenderer[] wheelComponentMeshRenderererArray;
 
 
     private void Start()
     {
         wheelCollider = GetComponent<WheelCollider>();
-        wheelTransform = GetComponentInChildren<MeshRenderer>().GetComponent<Transform>();
+        wheelComponentMeshRenderererArray = GetComponentsInChildren<MeshRenderer>();
     }
     private void Update()
     {
         wheelCollider.GetWorldPose(out Vector3 positionWheelCollider, out Quaternion rotationWheelCollider);
-        wheelTransform.position = positionWheelCollider;
-        wheelTransform.rotation = rotationWheelCollider;
+        foreach (MeshRenderer wheelComponent in wheelComponentMeshRenderererArray)
+        {
+            wheelComponent.transform.position = positionWheelCollider;
+            wheelComponent.transform.rotation = rotationWheelCollider;
+        }
     }
 
     private void FixedUpdate()
